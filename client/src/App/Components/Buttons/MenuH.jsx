c0055a91b97926e93ad3c823fba2857ac6e60469
HansBherer[Desktop] <77289920+HBherer@users.noreply.github.com>
2021-10-29 19:28:28 -0400

    Front-end dropdown menu and login screen

--- client/src/App/Components/Buttons/MenuH.jsx
+++ client/src/App/Components/Buttons/MenuH.jsx
@@ -1,24 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components"
-import MenuHamberger from "../../../Assets/Images/Icons/MenuHamberger.svg"
+import { Link } from "react-router-dom";
+import { ReactComponent as MenuIcon } from "../../../Assets/Images/Icons/MenuHamberger.svg";
+//import { ReactComponent as CloseMenu } from "../../../Assets/Images/Icons/close.svg";
+import { ReactComponent as Logout } from "../../../Assets/Images/Icons/Logout.svg";
+import { ReactComponent as Login } from "../../../Assets/Images/Icons/Login.svg";
 
-const BTNMenuHam = styled.button`
-   border: none;
-   background-color: transparent;
+const BTNMenuHam = styled.div`
+   padding: 7px 0px;
    cursor: pointer;
-   border: solid 1px transparent;
-   border-radius: 5px;
-   padding: 5px;
-   &:hover{
-       border: solid 1px #000000
-   }
 `;
+const ItemNavUser = styled.div`
+   list-style-type: none;
+`
+const ItemNavLink = styled.div`
+   text-decoration: none;
+`
+const UlNav = styled.ul`
+   padding-left: 0px;
+`
+const DropDown = styled.div`
+   position: absolute;
+   top: 75px;
+   right: -80px;
+   width: 200px;
+   transform: translateX(-40%);
+   background-color: #2F2F2C;
+   border: solid 1px #F0BB3C;
+   border-radius: 8px;
+   padding: 1rem;
+   overflow: hidden;
+`
+const MenuItem = styled(Link)`
+   display: flex;
+   height: 50px;
+   align-items: center;
+   color: #FFF6E5;
+   text-decoration: none;
+   font-size: 18px;
+   padding: 0px 15px;
+   &:hover{
+       background-color: #444440;
+       border-radius: 8px;
+   }
+`
+const SpanIconItem = styled.span`
+   margin-right: 10px;
+`
 
 const MenuH = () => {
     return (
         <BTNMenuHam>
-            <img src={MenuHamberger} alt="Logo Roasted Coffe club" />
+            <NavbarProfil>
+                <NavItem icon={<MenuIcon />} >
+                    <DropdownProfilMenu />
+                </NavItem>
+            </NavbarProfil>
         </BTNMenuHam>
+    )
+}
+
+
+function NavbarProfil(props) {
+    return (
+        <nav>
+            <UlNav>{props.children}</UlNav>
+        </nav>
+    );
+}
+function NavItem(props) {
+    const [open, setOpen] = useState(false);
+    return (
+        <ItemNavUser>
+            <ItemNavLink onClick={() => setOpen(!open)}>
+                {props.icon}
+            </ItemNavLink>
+            {open && props.children}
+        </ItemNavUser>
+    );
+}
+function DropdownProfilMenu() {
+    function DropdownItem(props) {
+        return (
+            <MenuItem to={props.linkTo}>
+                <SpanIconItem>{props.Icon1}</SpanIconItem>
+                {props.children}
+                <SpanIconItem>{props.Icon2}</SpanIconItem>
+            </MenuItem>
+        )
+    }
+    return (
+        <DropDown>
+            <DropdownItem linkTo="/" Icon1="💻"> Page 1</DropdownItem>
+            <DropdownItem linkTo="/login" Icon1={<Login />}> Connexion</DropdownItem>
+            <DropdownItem linkTo="/logout" Icon1={<Logout />}>Déconnexion</DropdownItem>
+        </DropDown>
     )
 }
 
---
+++ client/src/App/Screens/LoginScreen.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import NavHeader from "../Components/Header/NavHeader";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const MainLogin = styled.main`
+   display: flex;
+   justify-content: center;
+   @media (max-width: 960px) {
+    padding: 26px;
+  }
+`;
+const SectionLogin = styled.section`
+   display: flex;
+   justify-content: left;
+   flex-wrap: wrap;
+   width: 40%;
+   @media (max-width: 960px) {
+    width: 100%;
+  }
+`;
+const Headind3 = styled.h3`
+   width: 100%;
+`;
+const FormLogin = styled.form`
+   width: 100%;
+   margin-top: 24px;
+`;
+const LabelLogin = styled.label`
+   width: 100%;
+   font-size: 18px;
+   font-weight: bold;
+`;
+const InputLogin = styled.input`
+   width: 88%;
+   margin: 8px 0px 16px 0px;
+   font-family: 'Roboto', sans-serif;
+   background-color: #1F100B;
+   padding: 16px 22px;
+   border: none;
+   border-radius: 40px;
+   color: #D3B992;
+   margin-right: 20px;
+`;
+const ContainerEditBtnLogin = styled.div`
+   margin-top: 52px;
+   text-align: center;
+   @media (max-width: 960px) {
+    margin-top: 20px;
+  }
+`
+const BtnLogin = styled.button`
+   font-size: 18px;
+   font-weight: bold;
+   background-color: #D0C4A5;
+   border: none;
+   border-radius: 40px;
+   padding: 15px 5rem;
+   margin-top: 30px;
+`;
+
+const ContainerLogin = styled.div`
+   width: 100%;
+   text-align: center;
+   margin-top: 42px;
+`;
+const LinkToLogin = styled(Link)`
+   font-style: 18px;
+   font-family: 'Roboto', sans-serif;
+   font-weight: bold;
+   color: #000000;
+`;
+
+const LoginScreen = () => {
+    return (
+        <>
+            <NavHeader />
+            <MainLogin>
+                <SectionLogin>
+                    <Headind3>Rebonjour!</Headind3>
+                    <FormLogin>
+                        <LabelLogin htmlFor="LoginEmail">Adresse courriel</LabelLogin>
+                        <InputLogin type="email" id="LoginEmail" name="email" placeholder="votre@email.com" required />
+
+                        <LabelLogin htmlFor="LoginPassword">Mot de passe</LabelLogin>
+                        <InputLogin type="password" id="LoginPassword" name="password" placeholder="••••••••••••••" required />
+
+                        <ContainerEditBtnLogin>
+                            <BtnLogin type="submit">Connexion</BtnLogin>
+                        </ContainerEditBtnLogin>
+                    </FormLogin>
+                    <ContainerLogin>
+                        <p>Vous n’avez pas encore de compte?</p>
+                        <LinkToLogin to="/register">Inscrivez-vous!</LinkToLogin>
+                    </ContainerLogin>
+                </SectionLogin>
+            </MainLogin>
+        </>
+    )
+}
+
+export default LoginScreen;
