80fbd82c5b2bd87ff9c8a181b6258c7914bbec19
Zacharie-William Gagné <zwgagne@outlook.com>
2021-11-24 11:56:45 -0500

    fixed user invinting migration

--- server/controllers/meetController.js
+++ server/controllers/meetController.js
@@ -3,13 +3,12 @@ const pgClient = require("../db");
 module.exports = {
   sendMeetupRequest: async (req, res) => {    
     try {
-      res.status(200).json("Allo");
-      // const userInvited = req.params.friendName;
-      // const userInviting = req.user;
-      // const { date, time, address, place };
-
-      // await pgClient.query("INSERT INTO meetups (user_inviting, user_invited, meetup_date, meetup_time, meetup_address, meetup_place) VALUES ($1, $2, $3, $4, $5) RETURNING *", [userInviting, userInvited, date, time, address, place])
-      // res.status(200).json("Meetup créé");
+      const userInviting = req.user;
+      const { date, time, address, place } = req.body;
+      const userInvitedObject = await pgClient.query("SELECT user_id FROM users WHERE user_name = $1", [req.params.friendName])
+      const userInvited = userInvitedObject.rows[0].user_id; 
+      await pgClient.query("INSERT INTO meetups (user_inviting_id, user_invited, meetup_date, meetup_time, meetup_address, meetup_place) VALUES ($1, $2, $3, $4, $5) RETURNING *", [userInviting, userInvited, date, time, address, place])
+      res.status(200).json(userInvited);
       
     } catch (err) {
       console.error(err.message)
--- server/routes/meet.js
+++ server/routes/meet.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const meetController = require("../controllers/meetController");
 const authorization = require("../middleware/authorization");
 
-router.post("/send-meetup-request/:friendName", meetController.sendMeetupRequest)
+router.post("/send-meetup-request/:friendName", authorization, meetController.sendMeetupRequest)
 
 
 module.exports = router;
