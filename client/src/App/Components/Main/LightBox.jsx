de14fc33034f16f4723b7970f543f17901fb6378
HansBherer[Desktop] <77289920+HBherer@users.noreply.github.com>
2021-11-26 12:02:36 -0500

    Front-end | Styled creation meetup form

--- client/src/App/Components/Main/LightBox.jsx
+++ client/src/App/Components/Main/LightBox.jsx
@@ -11,18 +11,42 @@ const ContainerLightBox = styled.div`
    justify-content: left;
    width: 400px;
    padding: 26px;
-   top: 22%;
+   top: 10%;
    border-radius: 20px;
    @media (max-width: 503px) {
     width: 70%;
   }
+`;
+const ContainerBtnClose = styled.div`
+   display: flex;
+   width: 100%;
+   justify-content: right;
+`;
+const BtnCloseLB = styled.button`
+   border: none;
+   background-color: transparent;
+   font-size: 19px;
+   border-radius: 50px;
+   padding: 0px 10px;
+   height: 40px;
+   width: 40px;
+   @media (max-width: 503px) {
+    margin-left: 45px;
+    text-align: right;
+  }
+   &:hover{
+       background-color: #F6F5F1;
+   }
 `;
 
 const LightBox = (props) => {
     return (
         <>
             <ContainerLightBox>
-                <FormNewMeetUp functionLB3={props.functionLB2}/>
+                <ContainerBtnClose>
+                    <BtnCloseLB onClick={props.functionLB2} >X</BtnCloseLB>
+                </ContainerBtnClose>
+                <FormNewMeetUp functionLB3={props.functionLB2} />
             </ContainerLightBox>
         </>
     )
--- client/src/App/Components/Main/Meetup/FormNewMeetUp.jsx
+++ client/src/App/Components/Main/Meetup/FormNewMeetUp.jsx
@@ -1,36 +1,106 @@
 import React from "react";
 import styled from "styled-components";
+import AvatarIcon from "../../../../Assets/Images/Icons/Avatar_Icon_Profil.svg"
 
+const ContainerFrom = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+   padding: 10px 60px;
+`;
+const ContainerUserInvite = styled.div`
+   display: flex;
+   align-items: center;
+   margin-bottom: 30px;
+`;
+const SpanNameUI = styled.span`
+   margin-left: 10px;
+   font-size: 16px;
+`;
+const SectionFrom = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: left;
+`;
+const PInfoAction = styled.p`
+   font-weight: bold;
+   font-size: 16px;
+   margin-top: 0px;
+`;
 
+const LabelForm = styled.label`
+   font-weight: bold;
+   font-size: 16px;
+   width: 100%;
+`;
+const InputForm = styled.input`
+   font-size: 16px;
+   width: 100%;
+   background-color: transparent;
+   border: none;
+   border-bottom: solid 1px #000000;
+   margin-top: 10px;
+   margin-bottom: 15px;
+`;
 
+const ButtonSumitForm = styled.button`
+   width: 100%;
+   margin-top: 25px;
+   background-color: #E7D0A7;
+   text-decoration: none;
+   font-size: 18px;
+   font-weight: bold;
+   font-family: 'Roboto', sans-serif;
+   border: none;
+   padding: 15px 40px;
+   border-radius: 40px;
+   &:hover {
+     background-color: #EEDDBE;
+     box-shadow: 0px 3px 4px rgba(65, 40, 30, 0.25);
+     transition: all 0.2s ease;
+   }
+   &:active {
+    background: #E7D0A7;
+    box-shadow: inset 1px 2px 10px rgba(65, 40, 30, 0.15);
+    transition: all 0.2s ease;
+   }
+   &:disabled {
+    background: #EAE8E3;
+    color: #8B8B85;
+   }
+   &:visited {
+     color: inherit;
+   }
+`;
 
 const FormNewMeetUp = (props) => {
     return (
         <>
-            <h2>Nouveau Meetup</h2>
-            <p>Utilisateur invité</p>
-            <div>
-                <img alt="Avatar icon" />
-                <span>Edouard_Koffee</span>
-            </div>
-            <form>
+            <ContainerFrom>
+                <SectionFrom>
+                    <h2>Nouveau Meetup</h2>
+                    <PInfoAction>Utilisateur invité</PInfoAction>
+                    <ContainerUserInvite>
+                        <img src={AvatarIcon} alt="Avatar icon" />
+                        <SpanNameUI>Edouard_Koffee</SpanNameUI>
+                    </ContainerUserInvite>
+                    <form>
+                        <LabelForm htmlFor="placeMU">Lieu de rencontre</LabelForm>
+                        <InputForm type="text" id="placeMU" name="place" placeholder="Nom de l'etablissement" />
 
-                <label htmlFor="">Lieu de rencontre</label>
-                <input type="text" placeholder="Nom de l'etablissement" />
+                        <LabelForm htmlFor="adressMU">Adresse</LabelForm>
+                        <InputForm type="text" id="adressMU" name="adress" placeholder="Adresse et Ville" />
 
-                <label htmlFor="">Adresse</label>
-                <input type="text" placeholder="Adresse et Ville" />
+                        <LabelForm htmlFor="dateMU">Date de la rencontre</LabelForm>
+                        <InputForm type="date" id="dateMU" name="date" placeholder="" />
 
-                <label htmlFor="">Date de la rencontre</label>
-                <input type="date" placeholder="" />
+                        <LabelForm htmlFor="timeMU">Heure</LabelForm>
+                        <InputForm type="time" id="timeMU" name="time" placeholder="" />
 
-                <label htmlFor="">Heure</label>
-                <input type="time" placeholder="" />
-
-                <button type="submit" onClick={props.functionLB3}>Imposer une Meetup</button>
-
-
-            </form>
+                        <ButtonSumitForm type="submit" onClick={props.functionLB3}>Imposer une Meetup</ButtonSumitForm>
+                    </form>
+                </SectionFrom>
+            </ContainerFrom>
         </>
     )
 }
